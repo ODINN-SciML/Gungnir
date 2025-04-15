01f7e83b37a627d373ac9895d7d51d5cc52fb18c
Alban Gossard <alban.paul.gossard@gmail.com>
2025-04-15 12:15:28 +0200

    Add glacier names (#9)
    * add test and setup CI
    
    * simplify working_dir default behavior
    
    * fix typo
    
    * add build status badge
    
    * correct list of glaciers
    
    * add glacier names in the preprocessed directory
    
    ---------
    
    Co-authored-by: Alban Gossard <gossarda@univ-grenoble-alpes.fr>

--- gungnir/gungnir/preprocessing.py
+++ gungnir/gungnir/preprocessing.py
@@ -3,7 +3,7 @@ from oggm import cfg, utils, workflow, tasks, global_tasks
 from oggm.shop import bedtopo, millan22, glathida
 from MBsandbox.mbmod_daily_oneflowline import process_w5e5_data
 import json, os
-from gungnir.utils import read_glacier_names
+from gungnir.utils import read_glacier_names, remove_id_from_string
 
 _default_working_dir = utils.gettempdir('ODINN_prepro')
 
@@ -55,15 +55,19 @@ def preprocessing_glaciers(rgi_ids, working_dir=_default_working_dir):
 
     ### Then we retrieve all the necessary climate data ###
     rgi_paths = {}
+    rgi_names = {}
     for gdir in gdirs: # TODO: change to parallel processing by creating an entity task
         # We store all the paths for each RGI ID to be retrieved later on in ODINN
         rgi_paths[gdir.rgi_id] = gdir.dir.replace(working_dir+'/', '')
+        rgi_names[gdir.rgi_id] = remove_id_from_string(gdir.name)
         process_w5e5_data(gdir, climate_type='W5E5', temporal_resol='daily')
 
         print("dem path: " , gdir.get_filepath("dem"))
 
     with open(working_dir + '/rgi_paths.json', 'w') as f:
         json.dump(rgi_paths, f)
+    with open(working_dir + '/rgi_names.json', 'w') as f:
+        json.dump(rgi_names, f)
 
     # Verify that glaciers have no missing data
     task_log = global_tasks.compile_task_log(gdirs,
